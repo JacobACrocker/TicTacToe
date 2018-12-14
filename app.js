ce4ba96e690805583984cf8f9a3f01ffa232f72e
Jacob Crocker <jacobcrocker93@gmail.com>
2018-12-14 13:14:02 -0600

    Added message random players start

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+function randomizePlayer(){
+  var number = Math.floor(Math.random() * 10) +1;
+  if (number % 2 === 0) {
+    document.getElementById("message").innerHTML = "Player X goes first";
+    console.log(number + " is even");
+  }else {
+   document.getElementById("message").innerHTML = "Player O goes first";
+   console.log(number + " is odd");
+  }
+}
+
 function userMove(id) {
   document.getElementById(id).innerHTML = "X";
 }
@@ -8,4 +19,5 @@ function resetGame() {
   for (var i = 0; i < squares.length; i++) {
     squares[i].innerHTML = "";
   }
+  randomizePlayer();
 }
