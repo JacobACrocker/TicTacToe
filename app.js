3fdc843808e10a2d83d03933ce01f56452d853dd
Jacob Crocker <jacobcrocker93@gmail.com>
2018-12-14 22:45:31 -0600

    Added comments to clarify functions and added message updates

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
-//A random number between 1 and 10 will determine which player starts the game.
-//A message is then displayed indicating which player goes first.
+// A random number between 1 and 10 will determine which player starts the game.
+// A message is then displayed indicating which player goes first.
+// A value (either X or O) is assigned to a global variable called "player".
 function randomizeStartingPlayer(){
   var number = Math.floor(Math.random() * 10) + 1;
   if (number % 2 === 0) {
@@ -12,6 +13,22 @@ function randomizeStartingPlayer(){
     }
   }
 
+//This function will check to see is a player has already moved into the given cell.
+// if the cell is empty, the player (X or O) will populate the cell and the message will update.
+// If the cell is occupied, the cell will not update and a message will say it's occupied.
+function checkOccupiedCell(id){
+  if (document.getElementById(id).innerHTML === "") {
+    userDidMove(id);
+    currentPlayer();
+    message("It's " + player + "'s turn");
+  }else {
+    message("That cell is occupied. It is still " + player + "'s turn.");
+  }
+  }
+
+// The currentPlayer() function checks the global variable "player" value.
+// If the player's value is "X", the function changes it's value to "O" and vice-versa.
+// This function then updates the message with whose turn it is.
 function currentPlayer(){
   if (player === "X"){
     player = "O";
@@ -20,15 +37,18 @@ function currentPlayer(){
   }
 }
 
+// This is the function that directly handles the message update.
 function message(message){
   document.getElementById("message").innerHTML = message;
 }
 
+// this function updates the player mark inside each cell
 function userDidMove(id) {
   document.getElementById(id).innerHTML = player;
 }
 
-
+// The resetGame function resets the values that were placed by the
+// message() and userDidMove() functions.
 function resetGame() {
   var squares = document.getElementsByClassName("square");
   for (var i = 0; i < squares.length; i++) {
